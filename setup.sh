a03f8814632baa97453013cddab7e01f9a5bc36d
hounded <james.whiteman@gmail.com>
2018-06-06 23:08:55 +1200

    first commit

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+
+## INSTALL GIT
+sudo apt-get install git
+
+## INSTALL DOCKER
+sudo apt install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+## INSTALL TERRAFORM 
+wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
+unzip terraform_0.11.7_linux_amd64.zip
+rm terraform_0.11.7_linux_amd64.zip 
